6b5dea11c00561cd6a7635fb00aa3a70de88d68a
Ana Graca <ana.graca@kyndryl.com>
2022-05-03 21:31:20 +0100

    Criação do Menu onde podemos criar e editar vários mapas com pinos e editar os mesmos
    Nota: falta resolver problema de quando temos vários markers (ter pelo menos um selecionado)

---
+++ MyMap/MyMap/app/src/main/java/com/example/mymap/mymaps/models/Place.kt
@@ -0,0 +1,5 @@
+package com.example.mymap.mymmaps.models
+
+import java.io.Serializable
+
+data class Place(val title: String, val description: String, val latitude: Double, val longitude: Double) : Serializable
