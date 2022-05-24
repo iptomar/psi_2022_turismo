cc11d77110a17f75de6ca40756eb433d3e219cde
Ana Graca <Ana.Graca@kyndryl.com>
2022-05-24 23:25:16 +0100

    Formulário
    o findViewById está a fazr com que a app Crashe - é necessário perceber porquê

--- MyMaps/starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
+++ MyMaps/starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
@@ -4,10 +4,27 @@ package com.google.codelabs.buildyourfirstmap
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvCampoNome: EditText
+    private lateinit var tvCampoLatitude: EditText
+    private lateinit var tvcampoLongitude: EditText
+    private lateinit var tvcampoClassificacao: EditText
+    private lateinit var tvCampoProximidade: EditText
+    private lateinit var tvcampoDescricao: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_maps)
+
+        tvCampoNome = findViewById(R.id.tvCampoNome)
+        tvCampoLatitude = findViewById(R.id.tvCampoLatitude)
+        tvcampoLongitude = findViewById(R.id.tvcampoLongitude)
+        tvcampoClassificacao = findViewById(R.id.tvcampoClassificacao)
+        tvCampoProximidade = findViewById(R.id.tvCampoProximidade)
+        tvcampoDescricao = findViewById(R.id.tvcampoDescricao)
     }
+
+
 }
