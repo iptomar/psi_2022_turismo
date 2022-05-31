fde739a4e9e9bd1ada334526de5eed42ae17edd7
Ana Graca <Ana.Graca@kyndryl.com>
2022-05-31 20:11:44 +0100

    Preparação antes da navegação entre paginas

--- MyMaps/starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
+++ MyMaps/starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
@@ -1,15 +1,18 @@
 
 package com.google.codelabs.buildyourfirstmap
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import android.util.Log
+import android.widget.Button
+import android.widget.SeekBar
 import android.widget.TextView
+private const val TAG = "MainActivity"
+private const val TAG2 = "MainActivity"
+private const val INITIAL_DESCRIPTION = "Introduza uma descrição"
+private const val INITIAL_BAR = 50
 
 class MainActivity : AppCompatActivity() {
-
-    //@SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_maps)
@@ -20,8 +23,41 @@ class MainActivity : AppCompatActivity() {
         val tvcampoClassificacao: TextView = findViewById(R.id.tvcampoClassificacao)
         val tvCampoProximidade: TextView = findViewById(R.id.tvCampoProximidade)
         val tvcampoDescricao: TextView = findViewById(R.id.tvcampoDescricao)
+        val buttonAdd: Button = findViewById(R.id.buttonAdd)
+        val buttonCancel: Button = findViewById(R.id.buttonCancel)
+        val seekBarTip: SeekBar = findViewById(R.id.seekBarTip)
+
+
+        seekBarTip.progress = INITIAL_BAR
+        tvcampoClassificacao.text = "$INITIAL_BAR%"
+        tvcampoDescricao.text = INITIAL_DESCRIPTION
+        seekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.i(TAG2, "onProgressChanged $progress")
+                tvcampoClassificacao.text = "$progress%"
+                Log.i(TAG, "Localização ${tvCampoNome.text}")
+                Log.i(TAG, "Latitude ${tvCampoLatitude.text}")
+                Log.i(TAG, "Longitude ${tvcampoLongitude.text}")
+                Log.i(TAG, "Classificação ${tvcampoClassificacao.text}")
+                Log.i(TAG, "Proximidade ${tvCampoProximidade.text}")
+                Log.i(TAG, "Descrição ${tvcampoDescricao.text}")
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+
+        buttonAdd.setOnClickListener{
+            Log.i(TAG, "Localização ${tvCampoNome.text}")
+            Log.i(TAG, "Latitude ${tvCampoLatitude.text}")
+            Log.i(TAG, "Longitude ${tvcampoLongitude.text}")
+            Log.i(TAG, "Classificação ${tvcampoClassificacao.text}")
+            Log.i(TAG, "Proximidade ${tvCampoProximidade.text}")
+            Log.i(TAG, "Descrição ${tvcampoDescricao.text}")
+
+
+        }
     }
-
-
 }
-
---
+++ MyMaps/starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/files/Places.kt
@@ -0,0 +1,5 @@
+package com.google.codelabs.buildyourfirstmap.files
+
+class Places {
+
+}
